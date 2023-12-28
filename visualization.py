52491f48b7d634943e53d1570c6c8f84557ada4c
vasyllukian <91331245+vasyllukian@users.noreply.github.com>
2023-12-28 23:29:44 +0200

    Add files via upload

--- visualization.py
+++ visualization.py
@@ -1,5 +1,6 @@
 import psycopg2
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 
 username = 'postgres'
 password = 'postgres'
@@ -37,16 +38,19 @@ with conn:
     print("Database opened successfully")
     cur = conn.cursor()
 
+    fig = plt.figure(figsize=(16, 9))
+    gs = GridSpec(1, 3)
+
     # Запит 1. Вивести кількість годин прослуховування на день та рівень депресії
     cur.execute(query_1)
     hours_per_day, depression = zip(*cur)
 
+    ax1 = fig.add_subplot(gs[0, 0])
     plt.bar(hours_per_day, depression, width=0.3)
     plt.bar(hours_per_day, depression,color = 'red', width=0.3, alpha = 0.3)    
-    plt.xlabel('Кількість годин на день')
-    plt.ylabel('Рівень депресії')
-    plt.title('Кількість годин прослуховування на день та рівень депресії')
-    plt.show()
+    ax1.set_xlabel('Кількість годин на день')
+    ax1.set_ylabel('Рівень депресії')
+    ax1.set_title('Кількість годин прослуховування на день та рівень депресії')
 
 
     # Запит 2. Вивести улюблені жанри тих, у кого рівень тривоги менше 5
@@ -59,20 +63,22 @@ with conn:
     labels = genre_counts.keys()
     sizes = genre_counts.values()
 
-    plt.pie(sizes, autopct='%1.1f%%', startangle=90)
-    plt.legend(labels, loc='best')
-    plt.title('Улюблені жанри тих, у кого рівень тривоги менше 5')
-    plt.show()
+    ax2 = fig.add_subplot(gs[0, 1])
+    ax2.pie(sizes, autopct='%1.1f%%', startangle=90)
+    ax2.legend(labels)
+    ax2.set_title('Улюблені жанри тих, у кого рівень тривоги менше 5')
 
     # Запит 3. Вивести жанри та bpm музики для людей з середнім рівнем психічних захворювань більше 5, яким музика покращує психічний стан
     cur.execute(query_3)
     fav_genres, bpm_values = zip(*cur)
-
+    
+    ax3 = fig.add_subplot(gs[0, 2])
     plt.scatter(fav_genres, bpm_values)  
-    plt.xticks(fav_genres, ha='left', va='bottom')
-    plt.xlabel('Улюблений жанр')
-    plt.ylabel('BPM')
-    plt.title('Жанри та bpm музики для людей з середнім рівнем психічних \n захворювань більше 5, яким музика покращує психічний стан')
+    plt.xticks(fav_genres, ha='left', va='bottom') 
+    ax3.set_xlabel('Улюблений жанр')
+    ax3.set_ylabel('BPM')
+    ax3.set_title('Жанри та bpm музики для людей з середнім рівнем психічних \n захворювань більше 5, яким музика покращує психічний стан')
+    plt.savefig('graphs.png')
     plt.show()
 
 conn.close()
